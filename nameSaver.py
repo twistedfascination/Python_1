c86c081ba81cf223bdbaa369cc58d29ff8b4168b
TwistedFascination <Twisted.Fascination@gmail.com>
2020-05-13 15:17:01 -0700

    Python Test 2

---
+++ nameSaver.py
@@ -0,0 +1,8 @@
+userName = ""
+userAge = ""
+
+print("Hello, what is your name")
+userName = input()
+print("What is your age?")
+userAge= input()
+print("Your name is", userName, "and you are", userAge, "years old.").
